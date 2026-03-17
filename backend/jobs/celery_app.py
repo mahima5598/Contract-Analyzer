c9f7bfc7148cc0368823a9725737095821fcb045
Mahima Chhagani <chhaganimahima@gmail.com>
2026-03-16 22:24:36 -0400

    fix: workers for all tasks

--- backend/jobs/celery_app.py
+++ backend/jobs/celery_app.py
@@ -2,15 +2,19 @@
 import os
 from celery import Celery
 
-REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", REDIS_URL)
-CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", REDIS_URL)
+# REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+# CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", REDIS_URL)
+# CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", REDIS_URL)
+
+from celery import Celery
 
 celery_app = Celery(
-    "Contract-Analyzer",
-    broker=CELERY_BROKER_URL,
-    backend=CELERY_RESULT_BACKEND,
+    "backend.jobs",
+    broker="redis://localhost:6379/0",
+    backend="redis://localhost:6379/1",
 )
+
+celery_app.autodiscover_tasks(["backend.jobs"])
 
 # Basic recommended config
 celery_app.conf.update(
